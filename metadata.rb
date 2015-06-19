10c6579ca9874c576d5118c542704fadc4bf8d8b
Anthony Erlinger <aerlinger@gmail.com>
2015-06-19 00:24:35 -0400

    reference kibana_lwrp

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs and configures Kibana'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
-%w(git kibana).each do |cookbook|
+%w(git kibana_lwrp).each do |cookbook|
   depends cookbook
 end
